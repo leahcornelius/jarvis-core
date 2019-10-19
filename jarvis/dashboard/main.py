42f75eb9bd5ff754499449f8aa2739476c4eff33
Leo Cornelius <leocornelius019@gmail.com>
2019-10-19 16:09:41 +0100

    update dashboard/main.py

--- jarvis/dashboard/main.py
+++ jarvis/dashboard/main.py
@@ -1,1 +1,10 @@
+## dashboard/main.py - copywright 2019 Leo Cornelius @ The Jarvis Project
+##
+## This file manages the technical aspects of the defult page for both smart mirrors and smart screens
+## ( though both have UI differences ). This is the module run after tartup prodcedures. UI/index.html is
+## the main file for the ui with the js and css files being in the UI/js and UI/css respectivley. The dialog
+## and intent files are in speech/
+
+
+
 
