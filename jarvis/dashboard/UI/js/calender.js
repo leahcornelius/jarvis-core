17d333b4b5d1f7d706bf36a0fba21a8736ea3bb8
Leo Cornelius <leocornelius019@gmail.com>
2019-10-23 18:32:30 +0100

    Add Calender code
    Co-authored-by: nathan-www <nathan@nathanarnold.co.uk>

--- jarvis/dashboard/UI/js/calender.js
+++ jarvis/dashboard/UI/js/calender.js
@@ -3,6 +3,7 @@ function append(m) {
     var textContent = events + m + '\n';
    document.getElementById('events').innerHTML = textContent;
 }
+
 class calender_event {
     constructor(title = '', desc = '', eventLocation = '', time = '') {
         this.title = title;
@@ -11,16 +12,53 @@ class calender_event {
         this.time = time;
     }
 }
+
+class calender {
+    constructor(name='',events=['']) {
+        this.name = name;
+        this.events = events;
+    }
+}
+
 function addEvent(event) {
     var date = new Date(event.time*1000);
     time = date.toLocaleString('en-GB', { hour: 'numeric', minute: 'numeric', hour12: true });  
     var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     day = days[date.getDay()];
     append('<h2>'+day+'</h1>');
-    append('<h2>'+event.title+'</h2>');
+    append('<h2>'+eventc+'</h3>');
+    append('<h3>  '+event.e.title+'</h2>');
     append('<h3>  '+time+'</h3>');
-    append('<h3>  '+event.desc+'</h3>');
-    append('<h3>  '+event.eventLocation+'</h3>');
+    append('<h3>  '+event.desventLocation+'</h3>');
+}
+
+function getCalender() {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200){
+            //xmlHttp.responseText is response
+            addCalender(parseCalender(JSON.parse(this.responseText))); 
+        } 
+    }
+    xmlHttp.open("GET", 'http://' + config.jarvis_home_api_location+'/calender/v1?key='+config.jarvis_home_key, true); 
+    xmlHttp.send(null);
+}
+
+function parseCalender(calender_encoded) {
+    var i = 0;
+    var events;
+    for (i < calender_encoded.evenCount; i++;) {
+        events[i] = new calender_event(calender_encoded.events[i].title,calender_encoded.events[i].description,calender_encoded.events[i].location,calender_encoded.events[i].time);
+    }
+    var cal = new calender(calender_encoded.calenderName,events);
+    return cal;
+}
+
+function addCalender(cal) {
+    var i = 0;
+    for(i < cal.events.length; i++;) {
+        addEvent(cal.events[i]);
+    }  
 }
 
 function exampleEvent() {
